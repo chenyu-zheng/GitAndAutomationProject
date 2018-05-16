aa949e93ae4c4d60734201f83cbbee54816bde8e
chenyu-zheng <zcy9998@gmail.com>
2018-05-16 17:40:09 -0500

    Create image task using gulp-imagemin
    1. Add gulp-imagemin module.
    2. Create image task.
    3. Optimize image files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
 
 gulp.task('css', () => {
@@ -10,4 +11,10 @@ gulp.task('css', () => {
         }))
         .pipe(cleanCSS())
         .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('image', () => {
+    return gulp.src('src/images/**/*.+(png|jpg|gif)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
 });
